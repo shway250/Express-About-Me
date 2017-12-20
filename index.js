b6a1db95c280a0a7b7e55dfdd44f809cc4fd9734
philpatt <phillip.s.patterson1@gmail.com>
2017-12-19 19:38:00 -0800

    deliverable done

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
+	var dishes = fs.readFileSync('./data.json');
+	dishes = JSON.parse(dishes);
 	res.render('index', {
-		name: 'Phil',
-		dog: 'Rocky',
-		dish: ['Ramen', 'Red Curry', 'Double Cheeseburger', 'Triple Cheeseburger', 'Jumbo Shrimp']
+		mydishes: dishes,
+		name: "Phil"
 	});
 });
 
