f56fd62fb951c8a22f5db720373f3bb623715a95
Nick Nedev <nedevn@gmail.com>
2017-12-19 22:41:17 -0800

    Completed, no styles

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let fs = require('fs');
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/views'));
+
+//get JSON data from file
 let data = fs.readFileSync('data.json');
 data = JSON.parse(data);
+
 // Routes
 app.get('/', (req,res)=>{
 // res.send('good');
