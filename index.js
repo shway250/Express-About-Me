47559773f9a22391873028a5a35447157dbf5b36
peterpickles <lipeter2006@gmail.com>
2017-12-20 09:08:43 -0800

    adding name

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var express = require('express')
 var app = express();
+var path = require('path');
+
+
+app.get('/', function(req, res){
+	res.send("This is my about page!");
+});
+
+app.listen(3000);
