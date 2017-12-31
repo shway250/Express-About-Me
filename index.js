01d6691d99a15b4eef6809afb223bd5bb838018b
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2017-12-31 13:29:54 -0800

    reached mvp

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
-
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public/'));
+
+var obj = fs.readFileSync('data.json');
+obj = JSON.parse(obj);
+
 
 app.get('/', function(req, res){
 	res.render('home');
 });
+
+app.get('/winston', function(req, res){
+	res.render('winston', {obj:obj});
+})
 
 app.listen(3000);
