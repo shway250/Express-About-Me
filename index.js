6c246c6b26be06b950246ac715896849e25c08d2
peterpickles <lipeter2006@gmail.com>
2018-01-09 15:41:56 -0800

    initial setup for express page with routes

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express();
 var path = require('path');
+var bodyParser= require("body-parser");
 
+var fs = require("fs");
 
+//middleware to setup a directory so I can pull from it
+app.use(express.static(path.join(__dirname, "views")));
+
+//mora middleware so your app knows to use it on every request
+app.use(bodyParser.urlencoded({extended: false}));
+
+//setting frontend views to use ejs
+app.set("view engine", "ejs");
+
+//index route to grab the data.json
 app.get('/', function(req, res){
-	res.send("This is my about page!");
+	res.render('index')
+
 });
+
+app.post('/', function(res, req){
+
+	console.log(req.body);
+	res.send(req.body);
+})
 
 app.listen(3000);
