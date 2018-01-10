63cdb3b6895c43f448a50d37cb233a55237d9c3a
peterpickles <lipeter2006@gmail.com>
2018-01-10 10:50:34 -0800

    commiting so I don't lose progress

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-var express = require('express')
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
-var path = require('path');
-var bodyParser= require("body-parser");
-
-var fs = require("fs");
-
-//middleware to setup a directory so I can pull from it
-app.use(express.static(path.join(__dirname, "views")));
-
-//mora middleware so your app knows to use it on every request
-app.use(bodyParser.urlencoded({extended: false}));
 
 //setting frontend views to use ejs
-app.set("view engine", "ejs");
-
-//index route to grab the data.json
+app.set('view engine', 'ejs');
+//middleware to setup a directory so I can pull from it
+app.use(express.static(__dirname + '/public/'));
+app.use(ejsLayouts);
+//mora middleware so your app knows to use it on every request
+app.use(bodyParser.urlencoded({extended: false}));
+var object = 
+//home route to grab the data.json
 app.get('/', function(req, res){
-	res.render('index')
-
+	res.render("home");
 });
 
-app.post('/', function(res, req){
+app.get("/peter", function(req, res){
+	res.render("peter", {object:object});
 
-	console.log(req.body);
-	res.send(req.body);
-})
+});
 
 app.listen(3000);
