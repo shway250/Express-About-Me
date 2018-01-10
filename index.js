fe656582cfd3b3326809f4339caaaa6739225757
peterpickles <lipeter2006@gmail.com>
2018-01-10 11:25:28 -0800

    Got my data object to work

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ app.use(express.static(__dirname + '/public/'));
 app.use(ejsLayouts);
 //mora middleware so your app knows to use it on every request
 app.use(bodyParser.urlencoded({extended: false}));
-var object = 
+
+var object = fs.readFileSync("data.json");
+object = JSON.parse(object);
 //home route to grab the data.json
 app.get('/', function(req, res){
-	res.render("home");
-});
-
-app.get("/peter", function(req, res){
-	res.render("peter", {object:object});
-
+	res.render("home", {object:object});
 });
 
 app.listen(3000);
